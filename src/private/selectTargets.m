09fcead0aab3f1b81f722d735eb1bf872e37cd06
Chris Cox <crcox@wisc.edu>
2016-04-28 23:33:43 -0400

    handle missing targets when loading metadata (I don't have order data for some FacePlaceObject participants).

--- src/private/selectTargets.m
+++ src/private/selectTargets.m
@@ -6,10 +6,12 @@ function Y = selectTargets(metadata, type, label, source, metric, rowfilter)
   for i = 1:numel(metadata);
     t = metadata(i).targets;
     z = all([strcmpi(type,{t.type});strcmpi(label,{t.label});strcmpi(source,{t.sim_source});strcmpi(metric,{t.sim_metric})]);
-    if strcmpi(type, 'similarity')
-      Y{i} = metadata(i).targets(z).target(rowfilter{i}, rowfilter{i});
-    else
-      Y{i} = metadata(i).targets(z).target(rowfilter{i});
+    if any(z)
+      if strcmpi(type, 'similarity')
+        Y{i} = metadata(i).targets(z).target(rowfilter{i}, rowfilter{i});
+      else
+        Y{i} = metadata(i).targets(z).target(rowfilter{i});
+      end
     end
   end
 end
