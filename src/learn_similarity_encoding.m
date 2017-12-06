42642387bb3938cd92de8c0b016f91e98f41190d
Chris Cox cox.crc@gmail.com <christopher.cox-2@manchester.ac.uk>
2017-12-06 16:26:09 +0000

    probably finished trimming back, now it's time to get it all working again.

--- src/learn_similarity_encoding.m
+++ src/learn_similarity_encoding.m
@@ -1,4 +1,4 @@
-function [results,AdlasInstances] = learn_similarity_encoding(C, V, regularization, target_type, varargin)
+function [results,AdlasInstances] = learn_similarity_encoding(AdlasInstances, C, V, regularization, varargin)
 % TO DO:
 % Currently, the function can only handle a single subject. It will also
 % only support HYPERBAND for a single hyperparameter, meaning that it can
@@ -7,31 +7,15 @@ function [results,AdlasInstances] = learn_similarity_encoding(C, V, regularizati
 % be be "crossed" in the same way. With HYPERBAND, configurations can be
 % crossed with subject and cvholdout, and probably permutation index.
 
-    % Local Constants:
-    % ---------------
-    GLMNET_ALPHA_LASSO = 1;
-%     GLMNET_ALPHA_RIDGE = 0;
-
     p = inputParser();
+    addRequired(p  , 'AdlasInstances');
     addRequired(p  , 'C');
     addRequired(p  , 'V');
     addRequired(p  , 'regularization');
-    addRequired(p  , 'target_type');
-    addParameter(p , 'tau'                     , []       );
-    addParameter(p , 'lambda'                  , []       );
-    addParameter(p , 'lambda1'                 , []       );
-    addParameter(p , 'LambdaSeq'               , []       );
     addParameter(p , 'cvind'                   , []       );
-    addParameter(p , 'cvholdout'               , []       );
-    addParameter(p , 'normalize'               , []       );
-    addParameter(p , 'bias'                    , []       );
     addParameter(p , 'permutations'            , []       );
-    addParameter(p , 'DEBUG'                   , false    );
     addParameter(p , 'AdlasOpts'               , struct() );
-    addParameter(p , 'SmallFootprint'          , false    );
     addParameter(p , 'Verbose'                 , true     );
-    addParameter(p , 'PARALLEL'                , false    );
-    addParameter(p , 'AdlasInstances'          , []       );
     parse(p, C, V, regularization, target_type, varargin{:});
 
     if numel(p.Results.V) > 1
@@ -40,22 +24,16 @@ function [results,AdlasInstances] = learn_similarity_encoding(C, V, regularizati
     if numel(p.Results.lambda) == numel(p.Results.lambda1) && numel(p.Results.lambda) > 1
         warning('crcox:NotImplemented', 'Note that HYPERBAND is not yet implemented for GrOWL. Lambda and Lambda1 will be crossed, as if grid searching.');
     end
-    C                       = p.Results.C;
-    V                       = p.Results.V;
-    regularization          = p.Results.regularization;
-    lambda                  = p.Results.lambda;
-    lambda1                 = p.Results.lambda1;
-    LambdaSeq               = p.Results.LambdaSeq;
-    cvind                   = p.Results.cvind;
-    holdout                 = p.Results.cvholdout;
-    normalize               = p.Results.normalize;
-    permutations            = p.Results.permutations;
-    BIAS                    = p.Results.bias;
-    DEBUG                   = p.Results.DEBUG;
-    options                 = p.Results.AdlasOpts;
-    SMALL                   = p.Results.SmallFootprint;
-    VERBOSE                 = p.Results.Verbose;
-    PARALLEL                = p.Results.PARALLEL;
+    
+    AdlasInstances = p.Results.AdlasInstances;
+    C              = p.Results.C;
+    V              = p.Results.V;
+    regularization = p.Results.regularization;
+    cvind          = p.Results.cvind;
+    permutations   = p.Results.permutations;
+    options        = p.Results.AdlasOpts;
+    VERBOSE        = p.Results.Verbose;
+    
 
     if strcmp(regularization, {'grOWL','grOWL2'});
         assert(~isempty(LambdaSeq),'A LambdaSeq type (linear or exponential) must be set when using grOWL*');
@@ -63,28 +41,23 @@ function [results,AdlasInstances] = learn_similarity_encoding(C, V, regularizati
 
     Vorig = V;
 
-    if isempty(p.Results.AdlasInstances)
-        AdlasInstances = repmat(Adlas,N,1);
-    else
-        AdlasInstances = p.Results.AdlasInstances;
-    end
-    N = numel(AdlasInstances);
-
     if VERBOSE
         fprintf('%5s%6s%11s %11s  %11s %11s  \n', 'cv','lam','lam1','test err','train err','n vox')
     end
 
-    for i = 1:N
+    for i = 1:numel(AdlasInstances)
         subix = AdlasInstances(i).subject;
         permix = AdlasInstances(i).RandomSeed;
         cvix = AdlasInstances(i).cvholdout;
+        regularization = AdlasInstances(i).regularization;
+        normalize = AdlasInstances(i).normalize;
+        BIAS = AdlasInstances(i).bias;
         permutation_index = permutations{subix}(:,permix);
 
         test_set  = cvind{subix} == cvix; % CHECK THIS
         train_set = ~test_set;
 
         V = Vorig{subix};
-        % normalize (MAYBE MOVE THIS OUT OF MAIN LOOP?)
         switch lower(normalize)
             case 'zscore_train'
                 mm = mean(V(train_set,:),1);
@@ -121,9 +94,24 @@ function [results,AdlasInstances] = learn_similarity_encoding(C, V, regularizati
         if BIAS
             V = [V, ones(size(V,1),1)]; %#ok<AGROW> It's not actually growing, see line 86.
         end
-
         [~,d] = size(V);
 
+        switch upper(regularization)
+            case 'L1L2'
+                lamseq = lam;
+            case {'GROWL','GROWL2'} % There is no real distinction between GROWL and GROWL2 anymore, but for continuity I'll make GROWL2 map to this anyway.
+                switch lower(LambdaSeq)
+                    case 'linear'
+                        lamseq = lam1*(d:-1:1)/d + lam;
+                    case 'exponential'
+                        lamseq = lam*sqrt(2*log((d*ones(1,d))./(1:d)));
+                    case 'inf' % needs both lam and lam1
+                        lamseq = [lam+lam1, repmat(lam,1,d-1)];
+                end
+            otherwise
+                error('%s is not an implemented regularization. check spelling', regularization);
+        end
+        
         Cp = C{subix}(permutation_index,:);
         % cpcr = zeros(1,size(Cp,2));
         % for k = 1:size(Cp, 2);
@@ -133,215 +121,79 @@ function [results,AdlasInstances] = learn_similarity_encoding(C, V, regularizati
         Ct = Cp(train_set,:);
         Ch = Cp(test_set,:);
         Vt = V(train_set,:);
-        Vh = V(test_set,:);
                 
         if isempty(AdlasInstances(iii).Adlas)
         % In case of new model:
         %   1. Initialize model
         %   2. Train model
-            AdlasInstances(iii).Adlas = Adlas(Vt, Ct, lam, options);
+            AdlasInstances(iii).Adlas = Adlas(Vt, Ct, lamseq, options);
             AdlasInstances(iii).Adlas = AdlasInstances(iii).Adlas.train(options);
         elseif AdlasInstances(iii).status == 2
         % In case of existing model:
-        %   1. Check that status == 2,
-        %   which means that the previos
-        %   round of training stopped
-        %   because it hit the iteration
-        %   limit. 2. If so, train for more
-        %   iterations.
+        %   1. Check that status == 2, which means that the previos round
+        %   of training stopped because it hit the iteration limit.
+        %   2. If so, train for more iterations.
             AdlasInstances(iii).Adlas = AdlasInstances(iii).Adlas.train(options);
         else
-        % In case of existing model and
-        % status == 1 or status == 3,
-        % continue without doing anything.
-        % Status 1 means optimal
-        % convergence with at least one
-        % nonzero weight assigned.
-        % Status 3 means the solution is
-        % zero-sparse.
+        % In case of existing model and status == 1 or status == 3,
+        % continue without doing anything. Status 1 means optimal
+        % convergence with at least one nonzero weight assigned. Status 3
+        % means the solution is zero-sparse.
         %
-        % If a zero-sparse solution is
-        % obtained in a single iteration,
-        % this can prevent some information
-        % from ever being logged in the
-        % Adlas structure, which results in
-        % an error at run-time when trying
-        % to pick up where things left off.
+        % If a zero-sparse solution is obtained in a single iteration, this
+        % can prevent some information from ever being logged in the Adlas
+        % structure, which results in an error at run-time when trying to
+        % pick up where things left off.
         %
-        % By not trying to train these
-        % models any more (which is fine,
-        % because they have converged on a
-        % solution already, anyway), this
+        % By not trying to train these models any more (which is fine,
+        % because they have converged on a solution already, anyway), this
         % error should be avoided.
         end
 
         Uz = AdlasInstances(iii).Adlas.X;
         info = AdlasInstances(iii).Adlas;
-                
-                for j = 1:nlam
-                    if isempty(lambda)
-                        lam = nan(1);
-                    else
-                        lam = lambda(j);
-                    end
 
-                    for k = 1:nlam1
-                        iii = iii + 1;
-                        if isempty(lambda1)
-                            lam1 = nan(1);
-                        else
-                            lam1 = lambda1(k);
-                        end
+        % KEY
+        % ===
+        % Our models assume that S = V*W*V'
+        % V  : The fMRI data.
+        % W  : A matrix of weights.
+        % S  : The true similarity matrix.
+        % Sz : The predicted similarity matrix.
+        % C  : The square root of S, truncated to the first r columns (low rank assumption)
+        % Cz : The predicted C.
+        % St : The approximated S, reconstructed from actual C.
+        % Uz : The estimated voxel weights, with a r weights per voxel.
 
-                        if DEBUG
-                            Uz = randn(d,r);
-                            info.message = 'DEBUG';
-                            info.iter    = 0;
-                        else
-                            switch upper(regularization)
-                                case 'L1L2_GLMNET'
-                                    cvind_glmnet = drop_index_and_adjust(cvind{subix}, icv);
-                                    [Uz, info] = grouplasso_glmnet(Vt, Ct, ...
-                                        GLMNET_ALPHA_LASSO, ...
-                                        lam,...
-                                        'cvind'   , cvind_glmnet,...
-                                        'bias'    , BIAS, ...
-                                        'PARALLEL', PARALLEL, ...
-                                        'tol'     , 1e-8, ...
-                                        'U0'      ,   []);
+        nz_rows = any(Uz,2);
+        ix = find(nz_rows);
+        nv = size(Uz,1);
+        Unz = nnz(nz_rows);
+        uz = Uz(ix,:);
 
-                                case 'L1L2'
-                                    if all(lam==0)
-                                        Uz = pinv(Vt)*Ct;
-                                        info = struct();
-                                    else
-                                        [Uz, info] = Adlas1(Vt, Ct, lam, options);
-                                    end
+        % Prepare to evaluate solutions
+%         Wz = uz*uz';
+%         Sz = V(:,ix)*Wz*V(:,ix)';
+        Cz = V(:,ix)*uz;
+        Ctz = Cz(train_set,:);
+        Chz = Cz(test_set,:);
 
-
-                                case 'GROWL'
-                                    switch LambdaSeq
-                                        case 'linear'
-                                            lamseq = lam*(d:-1:1)/d + lam1;
-                                        case 'exponential'
-                                            lamseq = lam*sqrt(2*log((d*ones(1,d))./(1:d)));
-                                    end
-                                    if all(lamseq==0)
-                                        Uz = pinv(Vt)*Ct;
-                                        info = struct();
-                                    else
-                                        [Uz, info] = Adlas1(Vt, Ct, lamseq, options);
-                                    end
-
-                                case 'GROWL2'
-                                    switch LambdaSeq
-                                        case 'linear'
-                                            lamseq = lam*(d:-1:1)/d;
-                                        case 'exponential'
-                                            lamseq = lam*sqrt(2*log((d*ones(1,d))./(1:d)));
-                                        case 'inf' % needs both lam and lam1
-                                            lamseq = [lam+lam1, repmat(lam,1,d-1)];
-                                    end
-                                    if all(lamseq==0)
-                                        Uz = pinv(Vt)*Ct;
-                                        info = struct();
-                                    else
-                                        [Uz, info] = Adlas1(Vt, Ct, lamseq, options);
-                                    end
-                                otherwise
-                                    error('%s is not an implemented regularization. check spelling', regularization);
-                            end
-                        end
-
-                        % KEY
-                        % ===
-                        % Our models assume that S = V*W*V'
-                        % V  : The fMRI data.
-                        % W  : A matrix of weights.
-                        % S  : The true similarity matrix.
-                        % Sz : The predicted similarity matrix.
-                        % C  : The square root of S, truncated to the first r columns (low rank assumption)
-                        % Cz : The predicted C.
-                        % St : The approximated S, reconstructed from actual C.
-                        % Uz : The estimated voxel weights, with a r weights per voxel.
-
-                        nz_rows = any(Uz,2);
-                        ix = find(nz_rows);
-                        nv = size(Uz,1);
-                        Unz = nnz(nz_rows);
-                        uz = Uz(ix,:);
-
-                        % Prepare to evaluate solutions
-                        Wz = uz*uz';
-                        Sz = V(:,ix)*Wz*V(:,ix)';
-                        Cz = V(:,ix)*uz;
-                        Ctz = Vt(:,ix)*uz;
-                        Chz = Vh(:,ix)*uz;
-
-
-                        if ~SMALL
-                            results(iii).Uz  = uz;
-                            results(iii).Uix = uint32(ix(:)');
-                            results(iii).Cz  = Cz;
-                            results(iii).Sz  = Sz;
-                        end
-                        % Metadata
-                        results(iii).nzv            = uint32(Unz); % number of nonzero rows
-                        results(iii).nvox           = uint32(nv); % total number of voxels
-                        results(iii).subject        = subix;
-                        results(iii).cvholdout      = icv; % cross validation index
-                        results(iii).finalholdout   = []; % handled in parent function
-                        if strcmpi(regularization, 'L1L2_GLMNET')
-                            results(iii).lambda1    = info.lambda;
-                        else
-                            results(iii).lambda1    = lam1;
-                        end
-                        results(iii).lambda         = lam;
-                        results(iii).LambdaSeq      = LambdaSeq;
-                        results(iii).regularization = regularization;
-                        results(iii).bias           = BIAS;
-                        results(iii).normalize      = normalize;
-
-                        if any(test_set)
-                            results(iii).err1 = norm(Ch - Chz,'fro')/norm(Ch,'fro');
-                        end
-                        results(iii).err2 = norm(Ct - Ctz,'fro')/norm(Ct,'fro');
-
-                        if strcmpi(regularization, 'L1L2_GLMNET')
-                            results(iii).iter = info.npasses;
-                            info.iter = info.npasses;
-                        else
-                            results(iii).iter = info.iter;
-                        end
-
-                        if isempty(lambda)
-                            lambda_j = nan;
-                        else
-                            lambda_j = lambda(j);
-                        end
-                        if isempty(lambda1)
-                            lambda1_k = nan;
-                            if strcmpi(regularization, 'L1L2_GLMNET')
-                                lambda1_k = info.lambda;
-                            end
-                        else
-                            lambda1_k = lambda1(k);
-                        end
-
-                        err1 = results(iii).err1;
-                        err2 = results(iii).err2;
-                        if VERBOSE
-                            fprintf('%3d | %6.2f | %6.2f | %10.2f | %10.2f | %10d\n', ...
-                                icv, lambda_j,lambda1_k,err1,err2,Unz);
-                        end
-                    end % lam1 loop
-                end % lam loop
-            end % cv loop
+        if any(test_set)
+            err1 = nrsa_loss(Ch, Chz);
+            %cor1 = nrsa_corr(Ch*Ch', Chz*Chz');
+        else
+            err1 = [];
+            %cor1 = [];
         end
-    end % subject loop
-    if VERBOSE
-        fprintf('Exit status -- %s (%d iterations)\n', info.message, info.iter);
+        err2 = nrsa_loss(Ct, Ctz);
+        %cor2 = nrsa_corr(Ct*Ct', Ctz*Ctz');
+
+        if VERBOSE
+            fprintf('%3d | %6.2f | %6.2f | %10.2f | %10.2f | %10d | %10d\n', ...
+                icv,lam,lam1,err1,err2,Unz,nv);
+        end
     end
+    fprintf('Exit status -- %s (%d iterations)\n', info.message, info.iter);
 end % learn_similarity_encoding
 
 % OLD PERMUTATION ALGORITHM
@@ -383,4 +235,55 @@ end % learn_similarity_encoding
 %     s2  = s2(lt2);
 %     sz2 = sz2(lt2);
 %     st2 = st2(lt2);
+% end
+
+% COLLECT RESULTS
+% ===============
+% if ~SMALL
+%     results(iii).Uz  = uz;
+%     results(iii).Uix = uint32(ix(:)');
+%     results(iii).Cz  = Cz;
+%     results(iii).Sz  = Sz;
+% end
+% % Metadata
+% results(iii).nzv            = uint32(Unz); % number of nonzero rows
+% results(iii).nvox           = uint32(nv); % total number of voxels
+% results(iii).subject        = subix;
+% results(iii).cvholdout      = icv; % cross validation index
+% results(iii).finalholdout   = []; % handled in parent function
+% if strcmpi(regularization, 'L1L2_GLMNET')
+%     results(iii).lambda1    = info.lambda;
+% else
+%     results(iii).lambda1    = lam1;
+% end
+% results(iii).lambda         = lam;
+% results(iii).LambdaSeq      = LambdaSeq;
+% results(iii).regularization = regularization;
+% results(iii).bias           = BIAS;
+% results(iii).normalize      = normalize;
+% 
+% if any(test_set)
+%     results(iii).err1 = norm(Ch - Chz,'fro')/norm(Ch,'fro');
+% end
+% results(iii).err2 = norm(Ct - Ctz,'fro')/norm(Ct,'fro');
+% 
+% if isempty(lambda)
+%     lambda_j = nan;
+% else
+%     lambda_j = lambda(j);
+% end
+% if isempty(lambda1)
+%     lambda1_k = nan;
+%     if strcmpi(regularization, 'L1L2_GLMNET')
+%         lambda1_k = info.lambda;
+%     end
+% else
+%     lambda1_k = lambda1(k);
+% end
+% 
+% if strcmpi(regularization, 'L1L2_GLMNET')
+%     results(iii).iter = info.npasses;
+%     info.iter = info.npasses;
+% else
+%     results(iii).iter = info.iter;
 % end
